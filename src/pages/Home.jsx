378aee2dc4cf0169c2227485d9bfa4f18c380905
Vinch <vince.daviaud@gmail.com>
2023-12-01 04:51:56 +0100

    fix & add change animation on mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Parallax } from "react-parallax";
 import { Link } from "react-scroll";
 import AOS from "aos";
@@ -5,22 +6,37 @@ import "aos/dist/aos.css";
 
 export default function Home() {
   AOS.init({ duration: 2000 });
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div>
       <Parallax
         strength={200}
-        blur={3}
+        blur={4}
         bgImage="./assets/img/banner_image.jpg"
         bgImageAlt="studios entry"
-        className="h-[calc(100vh-110px)]"
+        className="h-[calc(100vh-110px)] flex items-center"
         bgImageStyle={{
           height: "100vh",
           objectFit: "cover",
           objectPosition: "bottom",
         }}
       >
-        <div className="w-[50%] translate-y-48 translate-x-16 flex flex-col gap-6">
+        <div className="md:w-[50%] max-md:px-5 md:translate-x-16 flex flex-col justify-center gap-6">
           <h2 className="text-3xl">
             Slow down in <br />
             Ahangama
@@ -38,15 +54,15 @@ export default function Home() {
         duration={2000}
         className="scroll-arrow"
       />
-      <div className="flex justify-center bg-quaternary">
+      <div className="flex justify-center overflow-hidden lg:px-12 bg-quaternary">
         <div
-          id="home-content"
-          className="p-3 mx-8 mt-24 mb-8 homegrid rounded-2xl bg-primary"
+          id="homegrid"
+          className="p-3 mx-3 mt-24 mb-8 md:mx-8 rounded-2xl bg-primary"
         >
           <div>
             <div className="flex h-full overflow-hidden rounded-tl-2xl">
               <img
-                className="object-cover w-full h-[90%] rounded-sm rounded-tl-2xl"
+                className="object-cover w-full h-[90%] rounded-sm rounded-tl-2xl max-md:rounded-t-2xl"
                 data-aos="fade-right"
                 src="./assets/img/west_room.jpg"
                 alt="west room"
@@ -68,7 +84,7 @@ export default function Home() {
           <div className="flex items-center justify-center">
             <div
               className="flex flex-col w-5/6 gap-3 h-fit"
-              data-aos="fade-right"
+              data-aos={isMobile ? "fade-left" : "fade-right"}
             >
               <h3>Private Terrace</h3>
               <p className="text-sm leading-relaxed font-ralewaylight">
@@ -83,16 +99,16 @@ export default function Home() {
             <div className="flex items-center h-full overflow-hidden">
               <img
                 className="object-cover w-full h-[90%] rounded-sm"
-                data-aos="fade-left"
+                data-aos={isMobile ? "fade-right" : "fade-left"}
                 src="./assets/img/terrace.jpeg"
                 alt="terrace"
               />
             </div>
           </div>
           <div>
-            <div className="flex items-end h-full overflow-hidden rounded-bl-2xl">
+            <div className="flex items-end h-full overflow-hidden md:rounded-bl-2xl">
               <img
-                className="object-cover w-full h-[90%] rounded-sm rounded-bl-2xl"
+                className="object-cover w-full h-[90%] rounded-sm md:rounded-bl-2xl"
                 data-aos="fade-right"
                 src="./assets/img/sunborn_east.jpeg"
                 alt="terrace"
